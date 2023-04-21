ee64c2ba5543fafb011f9434cbc501d1834be9fd
Hahn Axel (hahn) <axel.hahn@unibe.ch>
2023-04-21 11:27:28 +0200

    less output on shibd test

--- example/test_detect_shibboleth.php
+++ example/test_detect_shibboleth.php
@@ -7,10 +7,11 @@ $oAccess=new axelhahn\ahAccesscontrol();
 if(!$oAccess->detectUser()) {
     echo "NO USER user was detected. Now should follow a login form. ".PHP_EOL;
 } else {
-    echo "A user was detected:".PHP_EOL;
-    echo "    Type  : " . $oAccess->getUsertype().PHP_EOL;
-    echo "    userid: ".$oAccess->getUserid().PHP_EOL;
-    print_r($oAccess->auth->read());
+    echo "A user was detected.".PHP_EOL;
+    // echo "    userid: " . $oAccess->getUserid().PHP_EOL;
+    // echo "    Type  : " . $oAccess->getUsertype().PHP_EOL;
+    // print_r($oAccess->auth->read());
     
 }
 print_r($oAccess->dump());
+// print_r($_SERVER);
