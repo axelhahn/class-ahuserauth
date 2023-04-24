08d351130f8933887019efd67ff6cda3f768110d
Axel Hahn <ax2002@gmx.net>
2023-04-24 23:15:42 +0200

    add demo user

---
+++ data/auth/userfiles/user1.php
@@ -0,0 +1,13 @@
+<?php 
+return array (
+  'userid' => 'user1',
+  'uuid' => '',
+  'password' => '$2y$10$NwyDpZR5mZavNpPYi.vJoO9.nvOhFKUjAt2yStaZCjaku2imand9y',
+  'groups' => 
+  array (
+    0 => 'axelhahn\\ahAuthFile',
+  ),
+  'class' => 'axelhahn\\ahAuthFile',
+  'classfile' => '/home/axel/sources/php-classes/class-ahuserauth/classes/ah-auth-file.class.php',
+  'file' => './test_files.php',
+);
